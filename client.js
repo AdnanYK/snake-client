651f2d2c16cf0d7d93e5bfeea10987d66ed30dcc
AdnanYK <adnan.ykassab@gmail.com>
2022-01-19 19:36:30 +0000

    refactored play.js and added the name communication to client.js

--- client.js
+++ client.js
@@ -2,12 +2,21 @@ const net = require('net');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',// IP address here
+    host: '165.227.47.243',// IP address here
     port: 50541 //port number here
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: AYK');
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
 
   return conn;
 };
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-// const net = require('net');
+
 const {connect} = require('./client');
 
-// establish connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: 'localhost',// IP address here
-//     port: 50541 //port number here
-//   });
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
 
 console.log("Connecting ...");
-const connected = connect();
-
-// code that does something when the connection is first established
-connected.on('connect', () => {
-  connected.on('data', (data) => {
-    console.log(data);
-  });
-
-});
+connect();
