a99f12bb87a9f7a02537620e1ba8f44fabbab1d1
AdnanYK <adnan.ykassab@gmail.com>
2022-01-19 20:24:24 +0000

    added handleUSerInput to play to detect ctrl + c exit command

--- play.js
+++ play.js
@@ -5,3 +5,24 @@ const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin = setupInput();
+
+stdin.on("data", handleUserInput);
